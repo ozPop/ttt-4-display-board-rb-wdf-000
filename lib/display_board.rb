b58f4170af7acd0f7aa449cb77c2d3819c94f375
ozPop <ozziepopovas@gmail.com>
2016-06-15 16:59:09 -0400

    Done

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  border = "|"
+  separator = "-----------"
+  i = 0
+  output = ""
+  while i < board.length do
+    output += " #{board[i]} "
+    if i == 2 || i == 5
+      output += "\n#{separator}\n"
+    elsif i == 8
+      output += "\n"
+    else
+      output += "#{border}"
+    end
+    i += 1
+  end
+  puts output
+end
